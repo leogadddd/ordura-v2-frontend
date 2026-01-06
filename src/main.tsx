053d32982ce707fd7c4f586a7e0a788d3ab5f7eb
leogadddd <leogadil04@gmail.com>
2026-01-06 15:44:50 +0800

    change the location of the stylings

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { queryClient } from "./lib/queryClient";
 import App from "./App";
-import "./index.css";
+import "./styles/index.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 
